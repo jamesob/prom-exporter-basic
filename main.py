795ae5c36b833fb6e9dd023adae8a20424cc64d9
James O'Beirne <james.obeirne@pm.me>
2022-10-19 15:31:00 -0400

    Simplify webserver, add memory info

--- main.py
+++ main.py
@@ -2,93 +2,21 @@
 
 import subprocess
 import argparse
+import time
 import threading
-import sys
-import shutil
-import contextlib
-import socket
 import logging
-from io import BytesIO
+from wsgiref.simple_server import make_server
 
-from http import HTTPStatus
-from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 
 log = logging.getLogger(__name__)
 logging.basicConfig()
 
 
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    """Simple HTTP request handler with GET commands."""
-    server_version = "SimpleHTTP/1.0"
-
-    def do_GET(self):
-        """Serve a GET request."""
-        content: str = self.get_content()
-        f = BytesIO()
-        f.write(content.encode('utf-8'))
-        f.seek(0)
-
-        self.send_response(HTTPStatus.OK)
-        self.send_header("Content-type", 'text')
-        self.send_header("Content-Length", str(len(f.getbuffer())))
-        self.end_headers()
-
-        shutil.copyfileobj(f, self.wfile)
-
-    def get_content(self) -> str:
-        raise NotImplementedError
-
-
-# ensure dual-stack is not disabled; ref #38907
-class Server(ThreadingHTTPServer):
-
-    def server_bind(self):
-        # suppress exception when protocol is IPv4
-        with contextlib.suppress(Exception):
-            self.socket.setsockopt(
-                socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
-        return super().server_bind()
-
-    def finish_request(self, request, client_address):
-        self.RequestHandlerClass(request, client_address, self)
-
-
-def run_server(
-        bind='127.0.0.1', port=8000,
-        HandlerClass=BaseHTTPRequestHandler,
-        protocol="HTTP/1.0"
-):
-    """Run the HTTP request handler class.
-
-    This runs an HTTP server on port 8000 (or the port argument).
-
-    """
-    infos = socket.getaddrinfo(
-        bind, port,
-        type=socket.SOCK_STREAM,
-        flags=socket.AI_PASSIVE,
-    )
-    Server.address_family, _, _, _, addr = next(iter(infos))
-    HandlerClass.protocol_version = protocol
-
-    with Server(addr, HandlerClass) as httpd:
-        host, port = httpd.socket.getsockname()[:2]
-        url_host = f'[{host}]' if ':' in host else host
-        print(
-            f"Serving HTTP on {host} port {port} "
-            f"(http://{url_host}:{port}/) ..."
-        )
-        try:
-            httpd.serve_forever()
-        except KeyboardInterrupt:
-            print("\nKeyboard interrupt received, exiting.")
-            sys.exit(0)
-
-
-class MyReqHandler(SimpleHTTPRequestHandler):
-
-    def get_content(self):
-        return server_info()
+def wsgi_app(_, start_response):
+    status = '200 OK'
+    headers = [('Content-type', 'text/plain; charset=utf-8')]
+    start_response(status, headers)
+    return server_info()
 
 
 net_lock = threading.Lock()
@@ -109,7 +37,11 @@ class IfstatPoller(threading.Thread):
             inouts = line.split()
             try:
                 assert len(inouts) == len(devices) * 2
+            except AssertionError:
+                time.sleep(0.1)
+                continue
 
+            try:
                 with net_lock:
                     for i, d in enumerate(devices):
                         net_measurements[d] = inouts[2 * i:(2 * i) + 2]
@@ -118,36 +50,51 @@ class IfstatPoller(threading.Thread):
                 with net_lock:
                     net_measurements.clear()
 
+            time.sleep(0.1)
 
-def server_info() -> str:
+
+def server_info() -> list[bytes]:
     output = []
 
     def ao(line):
-        output.append(line)
+        output.append(line.encode() + b'\n')
 
-    hostname = socket.gethostname()
     df_lines = stdout('df | grep -v tmpfs').splitlines()[1:]
 
     for line in df_lines:
         dev, _, used, avail, useperc, mount = line.split()
         useperc = useperc.strip('%')
-        ao(f"disk_bytes_used{{host={hostname},mount={mount},device={dev}}} {used}")
-        ao(f"disk_bytes_avail{{host={hostname},mount={mount},device={dev}}} {avail}")
-        ao(f"disk_used_percent{{host={hostname},mount={mount},device={dev}}} {useperc}")
+        ao(f'disk_bytes_used{{mount="{mount}",device="{dev}"}} {used}')
+        ao(f'disk_bytes_avail{{mount="{mount}",device="{dev}"}} {avail}')
+        ao(f'disk_used_percent{{mount="{mount}",device="{dev}"}} {useperc}')
 
     load1, load5, load15 = stdout('cat /proc/loadavg').split()[:3]
 
-    ao(f"cpu_load_1min{{host={hostname}}} {load1}")
-    ao(f"cpu_load_5min{{host={hostname}}} {load5}")
-    ao(f"cpu_load_15min{{host={hostname}}} {load15}")
+    ao(f"cpu_load_1min {load1}")
+    ao(f"cpu_load_5min {load5}")
+    ao(f"cpu_load_15min {load15}")
+
+    total, used, free, *_ = stdout('free -m | tail +2').splitlines()[0].split()[1:]
+
+    ao(f"mem_total_mb {total}")
+    ao(f"mem_used_mb {used}")
+    ao(f"mem_free_mb {free}")
+    ao(f"mem_used_perc {int(used) * 100 / int(total):.3f}")
 
     if net_poller:
         with net_lock:
             for dev, (inkb, outkb) in net_measurements.items():
-                ao(f"net_KB_in{{host={hostname},device={dev}}} {inkb}")
-                ao(f"net_KB_out{{host={hostname},device={dev}}} {outkb}")
+                try:
+                    # Sometimes ifstat reports "n/a"
+                    float(inkb)
+                    float(outkb)
+                except ValueError:
+                    pass
+                else:
+                    ao(f'net_KB_in{{device="{dev}"}} {inkb}')
+                    ao(f'net_KB_out{{device="{dev}"}} {outkb}')
 
-    return '\n'.join(output)
+    return output
 
 
 def stdout(cmd) -> str:
@@ -166,7 +113,9 @@ def main():
         net_poller = IfstatPoller()
         net_poller.start()
 
-    run_server(args.bind, port=args.port, HandlerClass=MyReqHandler)
+    with make_server(args.bind, args.port, wsgi_app) as httpd:
+        print(f"serving on {args.bind}:{args.port}")
+        httpd.serve_forever()
 
 
 if __name__ == "__main__":
